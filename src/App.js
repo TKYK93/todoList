e86ff7789a5e89a67956c656314128f7a384d011
TKYK93 <laylalayla2905@gmail.com>
2020-10-19 23:09:24 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+
+function App() {
+  
+  const [inputText, setInputText] = useState(""); 
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  useEffect(()=>{
+    getLocalTodos();
+  }, []);
+  
+  useEffect(()=>{
+    　　　filterHandler();
+          saveLocalTodos();
+      }, [todos, status]);
+
+  const filterHandler=()=>{
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo=>todo.completed === true));
+        break;
+
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo=>todo.completed === false));
+        break;
+
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+  const saveLocalTodos = () => {
+      localStorage.setItem("todos",JSON.stringify(todos));
+      console.log("データが保存された");
+  };
+
+  // getLocalTodosはアプリが開始された時のみ呼び出し
+  const getLocalTodos = () => {
+    if(localStorage.getItem("todos") === null){
+      localStorage.setItem("todos", JSON.stringify([]));
+      console.log("保存データなし");
+    }else{
+      let localTodo = JSON.parse(localStorage.getItem("todos"));
+      setTodos(localTodo);
+      console.log("保存データあり");
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <Form 
+      todos={todos} 
+      setTodos={setTodos} 
+      inputText={inputText} 
+      setInputText = {setInputText}
+      setStatus = {setStatus}
+      />
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos={filteredTodos}/>
+    </div>
+  );
+}
+
+export default App;
